065840dab2e5f2970dd4375d54bdeb6eeddbaf06
JulienSuan <julienlemolo@gmail.com>
2024-02-10 19:05:22 +0100

    first commit

--- server/index.js
+++ server/index.js
@@ -33,35 +33,32 @@ app.get("/", (req, res) => {
     res.send("boo !")
 })
 
-
-app.post("/email", async(req, res) => {
-
-    const {para, nom, prénom, société} = req.body
-
-    console.log(req.body)
-
-
-    if (para, nom, prénom) {
-        
-        const file = fs.readFileSync(__dirname +"/mailTemplate/template.html")
-        const filecss = await inlineCss(file.toString(), {url: "file://" + __dirname + "/mailTemplate/"})
-        const templateCompiled = hogan.compile(filecss)
-        const templateRender = templateCompiled.render({para, nom, prénom, société})
-        
-        const message = {
-            from: "test@gmail.com",
-            to: "jbigot.dev@gmail.com",
-            subject: "Contact Portfolio",
-            html: templateRender
+app.post('/email', async (req, res) => {
+    try {
+      const { para, nom, prénom, société } = req.body;
+  
+      if (para && nom && prénom ) {
+        const file = fs.readFileSync(__dirname + '/mailTemplate/template.html');
+        const filecss = await inlineCss(file.toString(), { url: 'file://' + __dirname + '/mailTemplate/' });
+        const templateCompiled = hogan.compile(filecss);
+        const templateRender = templateCompiled.render({ para, nom, prénom, société });
+  
+        const mailOptions = {
+          from: 'votre_email@gmail.com',
+          to: 'jbigot.dev@gmail.com',
+          subject: 'Contact Portfolio',
+          html: templateRender
         };
-        
-        const info = await transporter.sendMail(message);
-        console.log("Message sent: %s", info.messageId);
-    
-        res.send("email envoyé")
-    } else {
-        res.send("il manque des informations")
+  
+        const info = await transporter.sendMail(mailOptions);
+        console.log('Message sent: %s', info.messageId);
+  
+        res.send('Email envoyé');
+      } else {
+        res.status(400).send('Il manque des informations dans le formulaire');
+      }
+    } catch (error) {
+      console.error('Erreur lors de l\'envoi de l\'email:', error);
+      res.status(500).send('Erreur lors de l\'envoi de l\'email');
     }
-
-})
-
+  });
--- web/src/components/Contact/Contact.js
+++ web/src/components/Contact/Contact.js
@@ -1,17 +1,78 @@
-import React, { useEffect, useRef } from 'react'
-import "./Contact.css"
+import React, { useEffect, useRef, useState } from 'react';
+import "./Contact.css";
 
-export default function Contact({setoffcontact,refresh}) {
+export default function Contact({ setoffcontact, refresh }) {
 
-    const containeeer = useRef(null)
+  const containeeer = useRef(null);
+  const [states, setstates] = useState({
+    nom: "",
+    prénom: "",
+    société: "",
+    para: ""
+  });
 
-    useEffect(() => {
-      
-        setoffcontact(containeeer.current.offsetTop)
-    }, [containeeer, refresh]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(states);
+
+
+    
+  };
+
+  useEffect(() => {
+    setoffcontact(containeeer.current.offsetTop);
+  }, [containeeer, refresh]);
+
   return (
     <div ref={containeeer} className='contact_container'>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <div>
+          <label htmlFor="lastname">Nom de famille</label>
+          <input
+            type="text"
+            value={states.nom}
+            onChange={(e) => setstates({ ...states, nom: e.target.value })}
+            id="lastname"
+            name="lastname"
+          />
+        </div>
 
+        <div>
+          <label htmlFor="firstname">Prénom</label>
+          <input
+            type="text"
+            value={states.prénom}
+            onChange={(e) => setstates({ ...states, prénom: e.target.value })}
+            id="firstname"
+            name="firstname"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="société">Société</label>
+          <input
+            type="text"
+            value={states.société}
+            onChange={(e) => setstates({ ...states, société: e.target.value })}
+            id="société"
+            name="société"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="message">Message</label>
+          <textarea
+            name="message"
+            value={states.message}
+            onChange={(e) => setstates({ ...states, message: e.target.value })}
+            id="message"
+            cols="30"
+            rows="10"
+          ></textarea>
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
     </div>
-  )
+  );
 }
